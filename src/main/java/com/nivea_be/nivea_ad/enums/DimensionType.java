711e7f863fe90f979c5318230cc2d4247efd4fd5
James Aduloju <adulojujames@gmail.com>
2025-02-14 09:06:55 +0100

    feat: completed dimension implementation

--- src/main/java/com/nivea_be/nivea_ad/enums/DimensionType.java
+++ src/main/java/com/nivea_be/nivea_ad/enums/DimensionType.java
@@ -7,7 +7,7 @@ public enum DimensionType {
     DIMENSION_320_480("dimension_320_480"),
     DIMENSION_480_320("dimension_480_320"),
     DIMENSION_300_600("dimension_300_600"),
-    DIMENSION_320_500("dimension_320_500"),
+    DIMENSION_320_500("dimension_320_50"),
     DIMENSION_300_250("dimension_300_250");
 
     private final String value;
